06447a344d709a58253a3995d139fa2d6a514c4d
Nik <nikhil.jagatia@evolve-consultants.co.uk>
2022-03-07 11:38:47 +0000

    first commit

---
+++ PaymentApp/src/app/about-us-page/about-us-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about-us-page',
+  templateUrl: './about-us-page.component.html',
+  styles: [
+  ]
+})
+export class AboutUsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
